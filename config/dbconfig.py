8f873f791e36bff5d828fde575b54605a6004290
AvirukBasak <avirukbasak@gmail.com>
2023-03-23 19:20:16 +0530

    fixed a syntax error

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -15,7 +15,8 @@ def dbconfig(app):
             os.path.join(
                 os.path.join(
                     os.path.dirname(__file__), '..'), 'data'), 'data.sqlite'))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://' + datapath
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + datapath
+    print(app.config['SQLALCHEMY_DATABASE_URI'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     SECRET_KEY = os.getenv('SECRET_KEY')
     if not SECRET_KEY: raise Exception('requires SECRET_KEY environment variable set')
