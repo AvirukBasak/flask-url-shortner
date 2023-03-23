0022f1f523d72ed93ab22b34346ecac87c0b015b
AvirukBasak <avirukbasak@gmail.com>
2023-03-23 19:43:24 +0530

    use setup.py to init db

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+def init_db():
+    if os.path.isfile('data/data.sqlite3'):
+        print('database already exists')
+        return
+
+    con = sqlite3.connect('data/data.sqlite3')
+    cur = con.cursor()
+
+    with open('dbmodels/schema_userdb.sql', 'r') as schema_userdb:
+        schema = schema_userdb.read()
+        cur.execute(schema)
+        print('created table userdb')
+
+    with open('dbmodels/schema_urldb.sql', 'r') as schema_urldb:
+        schema = schema_urldb.read()
+        cur.execute(schema)
+        print('created table urldb')
+
+    print('database initialised')
+    con.close()
+
+init_db()
